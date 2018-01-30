dd350cc8cde97132ded2513c3312a1461d8e644b
Ondrej Tomco <ondrej.tomco@akademiasovy.sk>
2018-01-30 08:12:19 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $("#loginBtn").click(verifyInput);
+
+    function verifyInput(){
+        
+        var userName = $("#usr").val();
+        var pass = $("#pass").val();
+
+        if(userName.length || pass.length )
+        
+    }
+
+
+
+
+
+});
