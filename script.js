10d3cc490fa07fb2d088e2b5780dda9643d24827
Ondrej Tomco <ondrej.tomco@akademiasovy.sk>
2018-02-01 13:56:31 +0100

    verification added

--- script.js
+++ script.js
@@ -6,9 +6,28 @@ $(function(){
         
         var userName = $("#usr").val();
         var pass = $("#pass").val();
-
-        if(userName.length || pass.length )
         
+        if(userName.length <1 || pass.length <1){
+            $("#warning").css("visibility", "visible");
+            console.log('hey');
+        }
+
+        else{
+            $("#warning").css("visibility","hidden" );
+
+            $.ajax({
+                url:"akademiasovy.ddns.net:8081/api/auth/login",
+                data: { format: 'json'},
+                error: function(){
+                   $("#error").html('not Found');
+                },
+                dataType: "json",
+                success : createTable,
+                type: 'POST'
+    
+            });
+        }
+
     }
 
 
